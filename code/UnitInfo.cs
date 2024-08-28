b02cb91935058b2ef8ce66ae6135f1a569e9a332
sleepjie <lugaruuuu@gmail.com>
2024-08-29 00:39:18 +0100

    fix
    fix : )

---
+++ code/UnitInfo.cs
@@ -0,0 +1,60 @@
+using Sandbox;
+
+public enum UnitType
+{
+	None,
+	Player,
+	Guard
+}
+
+public sealed class UnitInfo : Component
+{
+	[Property] public UnitType Team { get; set; }
+	[Property][Range(0.1f, 10f, 0.1f)] public float MaxHealth { get; set; }
+	public float Health { get; set; }
+    public bool Alive { get; private set; } = true;
+    public event Action<float> OnDamage;
+    public event Action OnDeath;
+    public float DelayDeath { get; set; } = 0f;
+
+    [Property][Range(0f, 2f, 0.1f)] TimeSince _lastDamage;
+    protected override void OnUpdate()
+	{
+
+    }
+
+    protected override void OnStart()
+    {
+        Health = MaxHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        if (!Alive) return;
+
+        Health = Math.Clamp(Health - damage, 0f, MaxHealth);
+
+        if (damage > 0)
+            _lastDamage = 0f;
+
+        OnDamage?.Invoke(damage);
+
+        if (Health <= 0)
+            Kill();
+    }
+
+    /// <summary>
+    /// Set the HP to 0 and Alive to false, then destroys it
+    /// </summary>
+    public async void Kill()
+    {
+        Health = 0f;
+        Alive = false;
+
+        OnDeath?.Invoke();
+
+        await Task.DelaySeconds(DelayDeath);
+
+        GameObject.Destroy();
+    }
+}
