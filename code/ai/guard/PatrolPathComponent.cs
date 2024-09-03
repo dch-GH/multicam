6254086bccc60701534da34b3468d0a707b6437c
duston <>
2024-09-03 13:15:00 -0500

    Remove EditorTool<PatrolPathComponent> for now

--- code/ai/guard/PatrolPathComponent.cs
+++ code/ai/guard/PatrolPathComponent.cs
@@ -5,12 +5,4 @@ public sealed class PatrolPathComponent : Component
 	[Property] public bool IsLoop { get; private set; }
 	public List<PatrolNode> Path { get; private set; }
 
-	// EDITOR ONLY
-	public bool IsBeingEdited = false;
-
-	[Button( "Edit Mode" )]
-	public void EnterEditMode()
-	{
-		IsBeingEdited = !IsBeingEdited;
-	}
 }
